7307232b8b6bc2e982ccae058288b6f174981fb4
unknown <volax6600mailpro@gmail.com>
2026-02-24 13:37:51 +0100

    first commit

---
+++ src/config.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+
+#include <vector>
---
+++ src/main.cpp
@@ -0,0 +1,105 @@
+#include "config.h"
+#include "triangle_mesh.h"
+
+unsigned int make_module(const std::string& filepath, unsigned int module_type){
+    std::ifstream file;
+    std::stringstream bufferedLines;
+    std::string line;
+
+    file.open(filepath);
+    while (std::getline(file, line)){
+        bufferedLines << line << "\n";
+    }
+    std::string shaderSource = bufferedLines.str();
+    const char* shaderSrc = shaderSource.c_str();
+    bufferedLines.str("");
+    file.close();
+
+    unsigned int shaderModule = glCreateShader(module_type);
+    glShaderSource(shaderModule, 1, &shaderSrc, NULL);
+    glCompileShader(shaderModule);
+
+    int success;
+    glGetShaderiv(shaderModule, GL_COMPILE_STATUS, &success);
+    if(!success){
+        char errorLog[1024];
+        glGetShaderInfoLog(shaderModule, 1024, NULL, errorLog);
+        std::cout << "Shader Module compilation error:\n" << errorLog << std::endl;
+    }
+
+    return shaderModule;
+}
+
+unsigned int make_shader(const std::string& vertex_filepath, const std::string& fragment_filepath){
+    std::vector<unsigned int> modules;
+    modules.push_back(make_module(vertex_filepath, GL_VERTEX_SHADER));
+    modules.push_back(make_module(fragment_filepath, GL_FRAGMENT_SHADER));
+
+    unsigned int shader = glCreateProgram();
+    for (unsigned int shaderModule : modules){
+        glAttachShader(shader, shaderModule);
+    }
+    glLinkProgram(shader);
+
+    int success;
+    glGetProgramiv(shader, GL_LINK_STATUS, &success);
+    if(!success){
+        char errorLog[1024];
+        glGetProgramInfoLog(shader, 1024, NULL, errorLog);
+        std::cout << "Shader Linking error:\n" << errorLog << std::endl;
+    }
+
+    for (unsigned int shaderModule : modules){
+        glDeleteShader(shaderModule);
+    }
+
+    return shader;
+}
+
+
+int main(){
+    GLFWwindow* window;
+
+    if(!glfwInit()){
+        std::cout << "GLFW couldn't load" << std::endl;
+        return -1; 
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    window = glfwCreateWindow(640, 480, "Qosmos Window", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)){
+        std::cout << "OpenGL couldn't load" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.25f, 0.5f, 0.75f, 1.0f);
+    int w,h;
+    glfwGetFramebufferSize(window, &w, &h);
+    glViewport(0,0,w,h);
+
+    TriangleMesh* triangle = new TriangleMesh();
+
+    unsigned int shader = make_shader(
+        "../src/shaders/vertex.txt",
+        "../src/shaders/fragment.txt"
+    );
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        glClear(GL_COLOR_BUFFER_BIT);
+        glUseProgram(shader);
+        triangle->draw();
+        glfwSwapBuffers(window);
+    }
+
+    glDeleteProgram(shader);
+    glfwTerminate();
+    return 0;
+}
